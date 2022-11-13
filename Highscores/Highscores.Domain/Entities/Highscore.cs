e0d68eddc2647dd141bc9fc75055ac6fdd64ff33
julioschuambach <julioschuambach.dev@gmail.com>
2022-11-13 15:38:44 -0300

    Criação da entidade Highscore

---
+++ Highscores/Highscores.Domain/Entities/Highscore.cs
@@ -0,0 +1,8 @@
+﻿namespace Highscores.Domain.Entities;
+
+public class Highscore
+{
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public string Player { get; private set; }
+    public int Score { get; private set; }
+}
