c3b1a14d177f0d9f9ae7d8db012a5eddf38254d8
julioschuambach <julioschuambach.dev@gmail.com>
2022-11-13 15:42:49 -0300

    Criação e configuração inicial da classe responsável pelo acesso à dados

---
+++ Highscores/Highscores.Infrastructure/Data/HighscoresDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Highscores.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Highscores.Infrastructure.Data;
+
+public class HighscoresDbContext : DbContext
+{
+    private readonly string _connectionString = "Server = localhost, 1433; Database = HighscoresDb; User ID = sa; Password = 1q2w3e4r@#$;";
+    public DbSet<Highscore> Highscores { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer(_connectionString);
+}
