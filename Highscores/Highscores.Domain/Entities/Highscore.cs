733ea36340525e09a5bd028263c7be689755d7f2
julioschuambach <julioschuambach.dev@gmail.com>
2023-09-18 11:42:35 -0300

    resource: created database via migration

--- Highscores/Highscores.Domain/Entities/Highscore.cs
+++ Highscores/Highscores.Domain/Entities/Highscore.cs
@@ -1,7 +1,9 @@
 ﻿using Highscores.Domain.Entities.Base;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Highscores.Domain.Entities;
 
+[Table("Highscores")]
 public class Highscore : EntityBase
 {
     public string Player { get; private set; }
---
+++ Highscores/Highscores.Infrastructure/Migrations/20230918143843_Highscores.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Highscores.Infrastructure.Migrations
+{
+    public partial class Highscores : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Highscores",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "UNIQUEIDENTIFIER", nullable: false),
+                    Player = table.Column<string>(type: "VARCHAR(15)", maxLength: 15, nullable: false),
+                    Score = table.Column<int>(type: "INT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Highscores_Id", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Highscores_Id",
+                table: "Highscores",
+                column: "Id",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Highscores");
+        }
+    }
+}
