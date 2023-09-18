37289e5b2b2cf128f05f4bba787a3899080025d3
julioschuambach <julioschuambach.dev@gmail.com>
2023-09-18 10:43:39 -0300

    feat: created Highscore entity

---
+++ Highscores/Highscores.Domain/Entities/Highscore.cs
@@ -0,0 +1,15 @@
+﻿using Highscores.Domain.Entities.Base;
+
+namespace Highscores.Domain.Entities;
+
+public class Highscore : EntityBase
+{
+    public string Player { get; private set; }
+    public int Score { get; private set; }
+
+    public Highscore(string player, int score)
+    {
+        Player = player;
+        Score = score;
+    }
+}
